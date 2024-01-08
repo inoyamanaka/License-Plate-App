84a89660a1f8906fdaaf90af3d3114e8347b90c4
Lambang <gilbertrafdi@gmail.com>
2024-01-08 10:16:09 +0700

    update configuration

--- GUI/pages/main_page.py
+++ GUI/pages/main_page.py
@@ -1,6 +1,23 @@
-import tkinter
+# Guide User Interface related
 import sys
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter.filedialog import askopenfile, asksaveasfile, askdirectory
+from tkinter.ttk import Progressbar
+from PIL import Image, ImageTk
+from prettytable import PrettyTable
+from tkinter import messagebox
+from tkinter.tix import IMAGETEXT
 
+# Image Preprocessing and OCR related
+import yolov5
+import cv2
+import easyocr
+import numpy as np
+from scipy import ndimage
+import re
+import math
 
 sys.path.append('D:/python/license-plate-application')
 from GUI.pages.home_page import HomePage
@@ -12,5 +29,5 @@ processor = LicensePlateProcessor()
 multi_processor = MultiLicensePlateProcessor()
 
 if __name__ == "__main__":
-    root = tkinter.Tk()
+    root = tk.tkinter.Tk()
     app = HomePage(root, processor, multi_processor)
